47f229c8f771ce5d3c8fdc09d393e83f9756d222
satisfly <vilkenbraadress@gmail.com>
2023-02-03 12:43:06 +0100

    Usings

--- Lms.Data/Data/AutoMapper/MapperProfile.cs
+++ Lms.Data/Data/AutoMapper/MapperProfile.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Lms.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using Lms.Core.DTOs;
 
 namespace Lms.Data.Data.AutoMapper
 {
-    internal class MapperProfile
+    internal class MapperProfile : Profile
     {
+
+        public MapperProfile()
+        {
+            CreateMap<Tournament, LmsDto>();
+        }
     }
 }
